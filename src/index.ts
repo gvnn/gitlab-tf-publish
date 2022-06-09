cbb9599f90523f5087926fe673a7af2f5e4b6d6e
Giovanni Ferron <giovanni.ferron@dpc.vic.gov.au>
2022-06-09 12:32:41 +1000

    feat: Add commands descriptions

--- src/index.ts
+++ src/index.ts
@@ -7,12 +7,14 @@ const program = new Command();
 
 const prepareCmd = program.command('prepare');
 prepareCmd
+  .description('Prepare a tar with contents of a module folder')
   .argument('<tag>', 'tf module tag')
   .argument('<src>', 'tf module folder')
   .action(prepare);
 
 const uploadCmd = program.command('upload');
 uploadCmd
+  .description('Upload file to gilab tf project registry')
   .argument('<tag>', 'tf module tag')
   .argument('<file>', 'filename')
   .action(upload);
